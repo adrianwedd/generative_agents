026453cc061102f63df417accd923a582569c642
adrianwedd <adrian@adrianwedd.com>
2023-08-16 23:39:14 +1000

    custom_simulation

---
+++ reverie/backend_server/utils.py
@@ -0,0 +1,16 @@
+# Copy and paste your OpenAI API Key
+openai_api_key = "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" #generative_agents
+# Put your name
+key_owner = "Adrian"
+
+maze_assets_loc = "../../environment/frontend_server/static_dirs/assets"
+env_matrix = f"{maze_assets_loc}/the_ville/matrix"
+env_visuals = f"{maze_assets_loc}/the_ville/visuals"
+
+fs_storage = "../../environment/frontend_server/storage"
+fs_temp_storage = "../../environment/frontend_server/temp_storage"
+
+collision_block_id = "32125"
+
+# Verbose 
+debug = True
